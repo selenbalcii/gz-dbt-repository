2b212f98cce3c86c9bf0f7b38b2b6a5768c7e022
Selen Balci <selenbalcii@gmail.com>
2025-12-03 21:56:21 +0000

    commit and sync

---
+++ models/staging/raw_data/stg_raw_data__raw_gz_ship.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('raw_data', 'raw_gz_ship') }}
+
+),
+
+renamed as (
+
+    select
+        orders_id,
+        shipping_fee,
+        shipping_fee_1,
+        logcost AS log_cost,
+		CAST(ship_cost AS INT64) AS ship_cost
+
+    from source
+
+)
+
+select * from renamed
